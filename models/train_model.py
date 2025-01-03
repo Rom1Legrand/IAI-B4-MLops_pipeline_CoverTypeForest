664325674c49d1cf24795a07e6129578dc072fe7
Rom1Legrand <legrandromain@gmail.com>
2025-01-03 20:42:35 +0100

    gestion erreur model_train.py

--- models/train_model.py
+++ models/train_model.py
@@ -1,31 +1,45 @@
+import os
 import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import boto3
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def train():
-    # Configuration MLflow
-    mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI']) # A VERIFIER
-    mlflow.set_experiment("forest_cover_type")
+    try:
+        # Configuration MLflow
+        mlflow.set_tracking_uri(os.environ['NEON_DATABASE_URL'])
+        mlflow.set_experiment("forest_cover_type")
+        logging.info("MLflow configuré")
 
-    # Charger les données
-    s3 = boto3.client('s3')
-    data_obj = s3.get_object(Bucket='rom1', Key='covertype/new_data/covtype_20.csv')
-    data = pd.read_csv(data_obj['Body'])
+        # Charger les données
+        s3 = boto3.client('s3')
+        bucket = os.environ['S3_BUCKET']
+        data_obj = s3.get_object(Bucket=bucket, Key='covertype/new_data/covtype_20.csv')
+        data = pd.read_csv(data_obj['Body'])
+        logging.info(f"Données chargées : {data.shape[0]} échantillons")
 
-    # Préparation des données
-    X = data.drop('target', axis=1)
-    y = data['target']
+        # Préparation des données
+        X = data.drop('target', axis=1)
+        y = data['target']
 
-    # Entrainement
-    with mlflow.start_run():
-        model = RandomForestClassifier(n_estimators=100)
-        model.fit(X, y)
-        
-        # Log métriques et modèle
-        mlflow.sklearn.log_model(model, "model")
-        mlflow.log_params(model.get_params())
-        mlflow.log_metric("accuracy", model.score(X, y))
+        # Entrainement
+        with mlflow.start_run():
+            model = RandomForestClassifier(n_estimators=100)
+            model.fit(X, y)
+            
+            accuracy = model.score(X, y)
+            logging.info(f"Accuracy: {accuracy}")
+
+            mlflow.sklearn.log_model(model, "model")
+            mlflow.log_params(model.get_params())
+            mlflow.log_metric("accuracy", accuracy)
+
+    except Exception as e:
+        logging.error(f"Erreur : {str(e)}")
+        raise
 
 if __name__ == "__main__":
     train()
