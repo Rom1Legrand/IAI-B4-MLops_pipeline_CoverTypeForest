4d4c9ea68a583fc8c7453682c236bdf616b45251
Rom1Legrand <legrandromain@gmail.com>
2025-01-04 10:21:16 +0100

    avec préparation des colonnes

--- models/train_model.py
+++ models/train_model.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 def train():
     try:
-        # Configuration MLflow avec PostgreSQL directement
+        # Configuration MLflow
         tracking_uri = os.environ['NEON_DATABASE_URL'].replace('postgresql', 'postgresql+psycopg2')
         mlflow.set_tracking_uri(tracking_uri)
         mlflow.set_experiment("forest_cover_type")
@@ -21,6 +21,16 @@ def train():
         data_obj = s3.get_object(Bucket=bucket, Key='covertype/new_data/covtype_20.csv')
         data = pd.read_csv(data_obj['Body'])
         logging.info(f"Données chargées : {data.shape[0]} échantillons")
+
+        # Vérifier et renommer la colonne cible si nécessaire
+        target_column = 'target'
+        if target_column not in data.columns:
+            # Vérifier les autres noms possibles comme 'Cover_Type', 'class', etc.
+            if 'Cover_Type' in data.columns:
+                data = data.rename(columns={'Cover_Type': 'target'})
+            # Ajouter d'autres conditions si nécessaire
+        
+        logging.info(f"Colonnes disponibles : {data.columns.tolist()}")
 
         # Préparation des données
         X = data.drop('target', axis=1)
