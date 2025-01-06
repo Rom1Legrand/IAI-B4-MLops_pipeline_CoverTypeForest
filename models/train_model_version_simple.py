a4e3765ccbd46c53489a25dc6d7c3014e9e16458
Rom1Legrand <legrandromain@gmail.com>
2025-01-06 19:17:32 +0100

    maj model et version mlflow pour jenkins

--- models/train_model_version_simple.py
+++ models/train_model_version_simple.py
@@ -1,24 +1,21 @@
 import os
 import boto3
-import pickle
 import logging
 import mlflow
 from dotenv import load_dotenv
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
-from sklearn.preprocessing import MinMaxScaler
 from sqlalchemy import create_engine, text
 import alembic.config
 import mlflow.store.db.utils
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Configuration des chemins
-current_dir = os.path.dirname(os.path.abspath(__file__))  # Dossier 'models'
-project_root = os.path.dirname(current_dir)  # Dossier racine du projet
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
 
 # Chemins vers les fichiers de configuration
 env_path = os.path.join(project_root, '.env')
@@ -51,28 +48,26 @@ def train():
         # Configuration MLflow
         mlflow.set_experiment("forest_cover_type")
         logging.info("MLflow configuré")
-
+        
         # Charger les données
         s3 = boto3.client('s3')
         bucket = os.environ['S3_BUCKET']
         data_obj = s3.get_object(Bucket=bucket, Key='covertype/new_data/covtype_20.csv')
         data = pd.read_csv(data_obj['Body'])
         logging.info(f"Données chargées : {data.shape[0]} échantillons")
-
+        
         # Vérifier et renommer la colonne cible si nécessaire
         target_column = 'target'
         if target_column not in data.columns:
-            # Vérifier les autres noms possibles comme 'Cover_Type', 'class', etc.
             if 'Cover_Type' in data.columns:
                 data = data.rename(columns={'Cover_Type': 'target'})
-            # Ajouter d'autres conditions si nécessaire
-        
+                
         logging.info(f"Colonnes disponibles : {data.columns.tolist()}")
-
+        
         # Préparation des données
         X = data.drop('target', axis=1)
         y = data['target']
-
+        
         # Entrainement
         with mlflow.start_run():
             model = RandomForestClassifier(n_estimators=50)
@@ -82,11 +77,11 @@ def train():
             f1 = f1_score(y, model.predict(X), average='weighted')  # weighted car problème multi-classe
             logging.info(f"Accuracy: {accuracy}")
             logging.info(f"F1 Score: {f1}")
-
-            mlflow.sklearn.log_model(model, "model")
+            
+            # Log uniquement les métriques et paramètres
             mlflow.log_params(model.get_params())
             mlflow.log_metric("accuracy", accuracy)
-            mlflow.log_metric("f1_score", f1)         
+            mlflow.log_metric("f1_score", f1)
             
     except Exception as e:
         logging.error(f"Erreur : {str(e)}")
